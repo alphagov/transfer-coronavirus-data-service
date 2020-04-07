624eaf86ac4906ca9e3a69af77637c95f4cad761
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2020-04-07 10:17:47 +0100

    Test get_group_by_name

--- tests/test_cognito_groups.py
+++ tests/test_cognito_groups.py
@@ -1,6 +1,7 @@
 import pytest
 
 from cognito_groups import (
+    get_group_by_name,
     get_group_list,
     get_group_map,
     return_users_group,
@@ -27,6 +28,17 @@ def test_get_group_list(standard_download, standard_upload):
     assert len(group_list) == 5
     assert standard_download in group_list
     assert standard_upload in group_list
+
+
+@pytest.mark.usefixtures("standard_download")
+@pytest.mark.usefixtures("standard_upload")
+def test_get_group_by_name(standard_download, standard_upload):
+
+    group = get_group_by_name("standard-download")
+    assert group == standard_download
+
+    group = get_group_by_name("standard-upload")
+    assert group == standard_upload
 
 
 @pytest.mark.usefixtures("standard_download")
