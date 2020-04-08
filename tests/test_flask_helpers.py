cba21b9587268ba7e321a139496f07c26e849361
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2020-04-08 15:56:05 +0100

    Add tests for is_admin_interface and is_development in flask_helpers.

--- tests/test_flask_helpers.py
+++ tests/test_flask_helpers.py
@@ -1,6 +1,8 @@
+import os
+
 import pytest
 
-from flask_helpers import has_upload_rights
+from flask_helpers import has_upload_rights, is_admin_interface, is_development
 from main import app
 
 
@@ -13,3 +15,21 @@ def test_route_index_logged_in(test_client, test_session):
     with app.test_request_context("/"):
         is_upload = has_upload_rights()
         assert not is_upload
+
+
+def test_is_admin_interface():
+    os.environ["ADMIN"] = "true"
+    assert is_admin_interface()
+    os.environ["ADMIN"] = "false"
+    assert not is_admin_interface()
+    del os.environ["ADMIN"]
+    assert not is_admin_interface()
+
+
+def test_is_development():
+    os.environ["FLASK_ENV"] = "development"
+    assert is_development()
+    os.environ["FLASK_ENV"] = "production"
+    assert not is_development()
+    del os.environ["FLASK_ENV"]
+    assert not is_development()
