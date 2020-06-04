8d4ebef00f2fe5d1d3300a303d8a0923ca092756
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2020-06-04 11:24:43 +0100

    Add a script to generate the s3paths.json file from the config in covid-engineering and provide instructions for users without access.

--- s3paths.sh
+++ s3paths.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-curdir=$(pwd)
-cd ../covid-engineering/reliability-engineering/terraform/modules/coronavirus-transfer-service-s3-config/local
-terraform init
-terraform plan
-pwd
-cp s3paths.json $curdir
-cd $curdir
+if [[ -d "../covid-engineering" ]]; then
+  curdir=$(pwd)
+  cd ../covid-engineering/reliability-engineering/terraform/modules/coronavirus-transfer-service-s3-config/local
+  terraform init
+  terraform plan
+  cp s3paths.json $curdir
+  cd $curdir
+else
+  echo "You need to checkout the alphagov/covid-engineering repository."
+  echo "Users without access can generate a file that matches: s3paths.example.json"
+fi
